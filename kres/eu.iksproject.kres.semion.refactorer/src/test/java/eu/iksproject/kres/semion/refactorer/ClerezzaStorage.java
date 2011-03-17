ca87e97f57d320e2cc2716e2b5373211898c08b1
Bonafede Concetto Elvio <concelvio@apache.org>
2011-03-17 12:49:48 +0000

    STANBOL-39: 1)  Removed dependency eu.iksproject.kres.ontologies from pom.xml file 2)  Added dependency org.apache.stanbol.reengineer.xml to the pom.xml 3) Changed references and imports in classes *.java from eu.iksproject.kres.ontologies to org.apache.stanbol.reengineer.xml
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1082473 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3c3b7e0262f5859ffff7743965ad1743d85db78f

--- kres/eu.iksproject.kres.semion.refactorer/src/test/java/eu/iksproject/kres/semion/refactorer/ClerezzaStorage.java
+++ kres/eu.iksproject.kres.semion.refactorer/src/test/java/eu/iksproject/kres/semion/refactorer/ClerezzaStorage.java
@@ -325,13 +325,15 @@ end_import
 
 begin_import
 import|import
-name|eu
+name|org
 operator|.
-name|iksproject
+name|apache
 operator|.
-name|kres
+name|stanbol
 operator|.
-name|ontologies
+name|reengineer
+operator|.
+name|xml
 operator|.
 name|XML_OWL
 import|;
