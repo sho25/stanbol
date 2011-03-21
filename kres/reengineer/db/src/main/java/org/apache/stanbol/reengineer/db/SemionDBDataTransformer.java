c3433deeb95b19691aa9194cd1746f931f3d6ec2
Bonafede Concetto Elvio <concelvio@apache.org>
2011-03-21 00:25:30 +0000

    STANBOL-39: 1) Changed depdency from eu.iksproject.kres.shared.trasformation to org.apache.stanbol.owlapi 2) Changed import  from eu.iksproject.kres.shared.trasformation to org.apache.stanbol.owlapi.trasformation.*
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1083629 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8f47ca2e03c482b269c3c615bf7ecd2ccd01bed4

--- kres/reengineer/db/src/main/java/org/apache/stanbol/reengineer/db/SemionDBDataTransformer.java
+++ kres/reengineer/db/src/main/java/org/apache/stanbol/reengineer/db/SemionDBDataTransformer.java
@@ -471,15 +471,15 @@ end_import
 
 begin_import
 import|import
-name|eu
+name|org
 operator|.
-name|iksproject
+name|apache
 operator|.
-name|kres
+name|stanbol
 operator|.
-name|shared
+name|owlapi
 operator|.
-name|transformation
+name|trasformation
 operator|.
 name|JenaToOwlConvert
 import|;
