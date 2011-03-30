57dfdc11c6eae586e113cb400169066e54bb0d91
Cihan Cimen <cihan@apache.org>
2011-03-30 13:43:53 +0000

    STANBOL-135: Initial import of ontologymanager/store
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1086954 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d5568827f009b820de31eef212c6c6530b5a8d15

---
+++ kres/ontologymanager/store/api/src/main/java/org/apache/stanbol/ontologymanager/store/api/StoreSynchronizer.java
@@ -0,0 +1,55 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|stanbol
+operator|.
+name|ontologymanager
+operator|.
+name|store
+operator|.
+name|api
+package|;
+end_package
+
+begin_comment
+comment|/**  * Interface for synchronizing the resources managed by {@link ResourceManager} with underlying  * {@link JenaPersistenceProvider} implementation  *   * @author Cihan  *   */
+end_comment
+
+begin_interface
+specifier|public
+interface|interface
+name|StoreSynchronizer
+block|{
+comment|/**      * Synchronizes all graphs that is stored in a {@link ResourceManager}      *       * @param force      *<p>      *            If set {@link ResourceManager} will be cleared and all resource-graph mappings will be      *            rebuilt.      *<p>      *            If not set the synchronizer should consider only deletion/addition of graphs.      */
+specifier|public
+name|void
+name|synchronizeAll
+parameter_list|(
+name|boolean
+name|force
+parameter_list|)
+function_decl|;
+comment|/**      * Synchronizes only specified graph. After synchronization the resource-graph mappings of the      * {@link ResourceManager} for the particular graph is synchronized      *       * @param graphURI      *            URI of the graph of which resources will be synchronized      */
+specifier|public
+name|void
+name|synchronizeGraph
+parameter_list|(
+name|String
+name|graphURI
+parameter_list|)
+function_decl|;
+comment|/**      * StoreSynchronizer is obtained through a factory and when its job is finished this method should be      * invoked to remove graph listeners.      */
+specifier|public
+name|void
+name|clear
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
