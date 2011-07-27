cb31fb816a2448f12491d7adff547c015a667a99
Alessandro Adamou <alexdma@apache.org>
2011-07-27 11:17:32 +0000

    STANBOL-302 : RuleStoreImpl constructor now passes non-null values in recursive constructor calls
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1151428 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0431623c8b0ad1344e7627367d6ec50f75b8dc4b

--- rules/manager/src/main/java/org/apache/stanbol/rules/manager/changes/RuleStoreImpl.java
+++ rules/manager/src/main/java/org/apache/stanbol/rules/manager/changes/RuleStoreImpl.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/* * Licensed to the Apache Software Foundation (ASF) under one or more * contributor license agreements.  See the NOTICE file distributed with * this work for additional information regarding copyright ownership. * The ASF licenses this file to You under the Apache License, Version 2.0 * (the "License"); you may not use this file except in compliance with * the License.  You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_comment
@@ -876,14 +876,6 @@ name|configuration
 return|;
 block|}
 specifier|private
-name|String
-name|alias
-decl_stmt|;
-specifier|private
-name|RuleParserImpl
-name|kReSRuleParser
-decl_stmt|;
-specifier|private
 specifier|final
 name|Logger
 name|log
@@ -963,9 +955,9 @@ block|{
 comment|// This recursive constructor call will also invoke activate()
 name|this
 argument_list|(
-literal|null
+name|onm
 argument_list|,
-literal|null
+name|configuration
 argument_list|,
 operator|(
 name|OWLOntology
