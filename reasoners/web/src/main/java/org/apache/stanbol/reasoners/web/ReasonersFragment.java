8e045b36556cb91224f69a68c6094935ae240748
Enrico Daga <enridaga@apache.org>
2011-08-31 17:12:43 +0000

    Reintegrating /branches/jena-reasoners/reasoners into /trunk/reasoners (STANBOL-185)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1163710 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 04a72a2a7fe4107107a8d72a5becc454dce39e6f

--- reasoners/web/src/main/java/org/apache/stanbol/reasoners/web/ReasonersFragment.java
+++ reasoners/web/src/main/java/org/apache/stanbol/reasoners/web/ReasonersFragment.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/* * Licensed to the Apache Software Foundation (ASF) under one or more * contributor license agreements.  See the NOTICE file distributed with * this work for additional information regarding copyright ownership. * The ASF licenses this file to You under the Apache License, Version 2.0 * (the "License"); you may not use this file except in compliance with * the License.  You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -231,11 +231,11 @@ name|stanbol
 operator|.
 name|reasoners
 operator|.
-name|base
+name|web
 operator|.
-name|api
+name|resources
 operator|.
-name|Reasoner
+name|ReasoningServiceTaskResource
 import|;
 end_import
 
@@ -253,7 +253,7 @@ name|web
 operator|.
 name|resources
 operator|.
-name|Classify
+name|ReasoningServicesResource
 import|;
 end_import
 
@@ -269,63 +269,9 @@ name|reasoners
 operator|.
 name|web
 operator|.
-name|resources
+name|writers
 operator|.
-name|ConsistencyCheck
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|stanbol
-operator|.
-name|reasoners
-operator|.
-name|web
-operator|.
-name|resources
-operator|.
-name|ConsistentRefactoring
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|stanbol
-operator|.
-name|reasoners
-operator|.
-name|web
-operator|.
-name|resources
-operator|.
-name|Enrichment
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|stanbol
-operator|.
-name|reasoners
-operator|.
-name|web
-operator|.
-name|resources
-operator|.
-name|ReasonersResource
+name|JenaModelWriter
 import|;
 end_import
 
@@ -394,7 +340,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Implementation of WebFragment for the Stanbol Reasoner end-point.  *   * @author Alberto Musetti  *  */
+comment|/**  * Implementation of WebFragment for the Stanbol Reasoner end-point.  *   * @author Alberto Musetti  *   */
 end_comment
 
 begin_class
@@ -445,11 +391,6 @@ name|String
 name|TEMPLATE_PATH
 init|=
 literal|"/org/apache/stanbol/reasoners/web/templates"
-decl_stmt|;
-annotation|@
-name|Reference
-name|Reasoner
-name|reasoners
 decl_stmt|;
 annotation|@
 name|Reference
@@ -509,11 +450,12 @@ argument_list|>
 argument_list|()
 decl_stmt|;
 comment|// Reasoner
+comment|//classes.add(ReasonersResource.class);
 name|classes
 operator|.
 name|add
 argument_list|(
-name|ReasonersResource
+name|ReasoningServicesResource
 operator|.
 name|class
 argument_list|)
@@ -522,34 +464,17 @@ name|classes
 operator|.
 name|add
 argument_list|(
-name|ConsistencyCheck
+name|ReasoningServiceTaskResource
 operator|.
 name|class
 argument_list|)
 expr_stmt|;
+comment|// Writer
 name|classes
 operator|.
 name|add
 argument_list|(
-name|ConsistentRefactoring
-operator|.
-name|class
-argument_list|)
-expr_stmt|;
-name|classes
-operator|.
-name|add
-argument_list|(
-name|Classify
-operator|.
-name|class
-argument_list|)
-expr_stmt|;
-name|classes
-operator|.
-name|add
-argument_list|(
-name|Enrichment
+name|JenaModelWriter
 operator|.
 name|class
 argument_list|)
