ec772ed4dfb3b90392e13d32d3a2d2ecc699214b
Alessandro Adamou <alexdma@apache.org>
2011-12-01 14:16:47 +0000

    Removed unneeded com.sun package reference introduced in latest STANBOL-186 fix (caused Eclipse build to fail but not Maven)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1209098 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c22066e3a8accd9bfe7d754461c403acdf8d86e3

--- rules/manager/src/main/java/org/apache/stanbol/rules/manager/atoms/IndividualPropertyAtom.java
+++ rules/manager/src/main/java/org/apache/stanbol/rules/manager/atoms/IndividualPropertyAtom.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/* * Licensed to the Apache Software Foundation (ASF) under one or more * contributor license agreements.  See the NOTICE file distributed with * this work for additional information regarding copyright ownership. * The ASF licenses this file to You under the Apache License, Version 2.0 * (the "License"); you may not use this file except in compliance with * the License.  You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -26,6 +26,22 @@ operator|.
 name|util
 operator|.
 name|ArrayList
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|stanbol
+operator|.
+name|rules
+operator|.
+name|base
+operator|.
+name|SWRL
 import|;
 end_import
 
@@ -334,36 +350,6 @@ operator|.
 name|rulesys
 operator|.
 name|Node_RuleVariable
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|stanbol
-operator|.
-name|rules
-operator|.
-name|base
-operator|.
-name|SWRL
-import|;
-end_import
-
-begin_import
-import|import
-name|sun
-operator|.
-name|tools
-operator|.
-name|jar
-operator|.
-name|resources
-operator|.
-name|jar
 import|;
 end_import
 
@@ -1355,7 +1341,7 @@ argument_list|,
 literal|""
 argument_list|)
 expr_stmt|;
-comment|//subjectNode = Node_RuleVariable.createVariable(subject);
+comment|// subjectNode = Node_RuleVariable.createVariable(subject);
 name|subjectNode
 operator|=
 operator|new
@@ -1506,7 +1492,7 @@ argument_list|,
 literal|""
 argument_list|)
 expr_stmt|;
-comment|//predicateNode = Node_RuleVariable.createVariable(predicate);
+comment|// predicateNode = Node_RuleVariable.createVariable(predicate);
 name|predicateNode
 operator|=
 operator|new
