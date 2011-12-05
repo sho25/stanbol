5e0d1e7959c1525cca37214bd9dad3a3e3192d74
Rupert Westenthaler <rwesten@apache.org>
2011-12-05 22:35:22 +0000

    STANBOL-409: Added Implementation for the RDFBackend of LDPath. This also includes some changes to the Entityhub such as updated ModelUtils; The usage of LinkedHashSet instead of ArrayLists for QueryResults;
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1210685 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 37e9e38a64e9886df6d354e189038d0b45ddf697

---
+++ entityhub/ldpath/src/test/java/org/apache/stanbol/entityhub/ldpath/impl/LdPathTest.java
@@ -0,0 +1,26 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|stanbol
+operator|.
+name|entityhub
+operator|.
+name|ldpath
+operator|.
+name|impl
+package|;
+end_package
+
+begin_class
+specifier|public
+class|class
+name|LdPathTest
+block|{  }
+end_class
+
+end_unit
+
