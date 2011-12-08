0f5af067ac5e38432e696705e14d03521ef77cdf
Suat Gonul <suat@apache.org>
2011-12-08 13:26:36 +0000

    STANBOL-348: getStaticRootUrl() of BaseStanbolResource now returns the full URI of the static resource so that the static resources are correctly accessed by browsers. I had explained the problem at [1]. Also, when war file is deployed to application server, it starts automatically now.
    Furthermore, static resources are deployed considering the configured web.alias property instead of considering only web.static.url property.
    
    As far as I tried, configuring web.alias and/or web.static.url properties works as expected in war context and other launchers.  
    
    [1] http://markmail.org/message/e25iczswtvtf5a7g
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1211871 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 32f431d72ca097c0d7a4d25e7248bbfa1d6e80f1

--- commons/web/base/src/main/java/org/apache/stanbol/commons/web/base/JerseyEndpoint.java
+++ commons/web/base/src/main/java/org/apache/stanbol/commons/web/base/JerseyEndpoint.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/* * Licensed to the Apache Software Foundation (ASF) under one or more * contributor license agreements.  See the NOTICE file distributed with * this work for additional information regarding copyright ownership. * The ASF licenses this file to You under the Apache License, Version 2.0 * (the "License"); you may not use this file except in compliance with * the License.  You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -603,7 +603,7 @@ name|componentContext
 operator|=
 name|ctx
 expr_stmt|;
-comment|//init corsOrigins
+comment|// init corsOrigins
 name|Object
 name|values
 init|=
@@ -838,6 +838,22 @@ argument_list|(
 name|STATIC_RESOURCES_URL_ROOT_PROPERTY
 argument_list|)
 decl_stmt|;
+name|String
+name|applicationAlias
+init|=
+operator|(
+name|String
+operator|)
+name|componentContext
+operator|.
+name|getProperties
+argument_list|()
+operator|.
+name|get
+argument_list|(
+name|ALIAS_PROPERTY
+argument_list|)
+decl_stmt|;
 comment|// incrementally contribute fragment resources
 name|List
 argument_list|<
@@ -976,6 +992,31 @@ block|{
 name|String
 name|resourceAlias
 init|=
+operator|(
+name|applicationAlias
+operator|.
+name|endsWith
+argument_list|(
+literal|"/"
+argument_list|)
+condition|?
+name|applicationAlias
+operator|.
+name|substring
+argument_list|(
+literal|0
+argument_list|,
+name|applicationAlias
+operator|.
+name|length
+argument_list|()
+operator|-
+literal|1
+argument_list|)
+else|:
+name|applicationAlias
+operator|)
+operator|+
 name|staticUrlRoot
 operator|+
 literal|'/'
@@ -1038,22 +1079,6 @@ operator|new
 name|ServletContainer
 argument_list|(
 name|app
-argument_list|)
-decl_stmt|;
-name|String
-name|applicationAlias
-init|=
-operator|(
-name|String
-operator|)
-name|componentContext
-operator|.
-name|getProperties
-argument_list|()
-operator|.
-name|get
-argument_list|(
-name|ALIAS_PROPERTY
 argument_list|)
 decl_stmt|;
 name|Bundle
--- commons/web/base/src/main/java/org/apache/stanbol/commons/web/base/resource/BaseStanbolResource.java
+++ commons/web/base/src/main/java/org/apache/stanbol/commons/web/base/resource/BaseStanbolResource.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/* * Licensed to the Apache Software Foundation (ASF) under one or more * contributor license agreements.  See the NOTICE file distributed with * this work for additional information regarding copyright ownership. * The ASF licenses this file to You under the Apache License, Version 2.0 * (the "License"); you may not use this file except in compliance with * the License.  You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -442,7 +442,30 @@ name|String
 name|getStaticRootUrl
 parameter_list|()
 block|{
+name|String
+name|baseURI
+init|=
+name|getPublicBaseUri
+argument_list|()
+operator|.
+name|toString
+argument_list|()
+decl_stmt|;
 return|return
+name|baseURI
+operator|.
+name|substring
+argument_list|(
+literal|0
+argument_list|,
+name|baseURI
+operator|.
+name|length
+argument_list|()
+operator|-
+literal|1
+argument_list|)
+operator|+
 operator|(
 name|String
 operator|)
