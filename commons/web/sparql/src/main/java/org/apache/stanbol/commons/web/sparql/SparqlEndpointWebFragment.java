5cfcbe0b634c1503945abd81181955474496eab5
Suat Gonul <suat@apache.org>
2012-04-09 15:43:40 +0000

    STANBOL-580: -Removed Contenthub dependency of SPARQL endpoint. Now, it is possible to execute SPARQL query on any TripleCollection registered to OSGi environment.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1311286 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 86230289fc5536cf25b73999f23013f519d15443

--- commons/web/sparql/src/main/java/org/apache/stanbol/commons/web/sparql/SparqlEndpointWebFragment.java
+++ commons/web/sparql/src/main/java/org/apache/stanbol/commons/web/sparql/SparqlEndpointWebFragment.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/* * Licensed to the Apache Software Foundation (ASF) under one or more * contributor license agreements.  See the NOTICE file distributed with * this work for additional information regarding copyright ownership. * The ASF licenses this file to You under the Apache License, Version 2.0 * (the "License"); you may not use this file except in compliance with * the License.  You may obtain a copy of the License at * *     http://www.apache.org/licenses/LICENSE-2.0 * * Unless required by applicable law or agreed to in writing, software * distributed under the License is distributed on an "AS IS" BASIS, * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. * See the License for the specific language governing permissions and * limitations under the License. */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -75,24 +75,6 @@ name|org
 operator|.
 name|apache
 operator|.
-name|clerezza
-operator|.
-name|rdf
-operator|.
-name|core
-operator|.
-name|access
-operator|.
-name|TcManager
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
 name|felix
 operator|.
 name|scr
@@ -116,22 +98,6 @@ operator|.
 name|annotations
 operator|.
 name|Component
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|felix
-operator|.
-name|scr
-operator|.
-name|annotations
-operator|.
-name|Reference
 import|;
 end_import
 
@@ -263,24 +229,6 @@ begin_import
 import|import
 name|org
 operator|.
-name|apache
-operator|.
-name|stanbol
-operator|.
-name|contenthub
-operator|.
-name|servicesapi
-operator|.
-name|store
-operator|.
-name|Store
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
 name|osgi
 operator|.
 name|framework
@@ -371,17 +319,6 @@ decl_stmt|;
 specifier|private
 name|BundleContext
 name|bundleContext
-decl_stmt|;
-comment|// put references to the required OSGi services
-annotation|@
-name|Reference
-name|TcManager
-name|tcManager
-decl_stmt|;
-annotation|@
-name|Reference
-name|Store
-name|store
 decl_stmt|;
 annotation|@
 name|Override
