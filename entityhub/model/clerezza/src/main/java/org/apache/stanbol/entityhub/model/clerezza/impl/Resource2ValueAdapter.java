929683d98837374363c9173ef7210d7542b5eaf0
Rupert Westenthaler <rwesten@apache.org>
2012-07-23 06:51:06 +0000

    fixes STANBOL-698 by catching RuntimeExceptions while converting TypedLiterals to Java Objects and using the lexical form (String value) in case of an Exception.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/stanbol/trunk@1364515 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4a027f15829c3023fe2fffe27bc96671fad91683

--- entityhub/model/clerezza/src/main/java/org/apache/stanbol/entityhub/model/clerezza/impl/Resource2ValueAdapter.java
+++ entityhub/model/clerezza/src/main/java/org/apache/stanbol/entityhub/model/clerezza/impl/Resource2ValueAdapter.java
@@ -232,7 +232,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Converts the Resources (used to store field values in the Clerezza triple  * store) back to values as defined by the {@link Representation} interface  * @author Rupert Westenthaler  *  * @param<T> the type of the Resource that can be converted to values  */
+comment|/**  * Converts the Resources (used to store field values in the Clerezza triple store) back to values as defined  * by the {@link Representation} interface  *   * @author Rupert Westenthaler  *   * @param<T>  *            the type of the Resource that can be converted to values  */
 end_comment
 
 begin_class
@@ -367,8 +367,8 @@ operator|==
 literal|null
 condition|)
 block|{
-comment|//if no dataType is defined
-comment|//return a Text without a language
+comment|// if no dataType is defined
+comment|// return a Text without a language
 return|return
 name|valueFactory
 operator|.
@@ -412,6 +412,8 @@ operator|!=
 literal|null
 condition|)
 block|{
+try|try
+block|{
 return|return
 name|literalFactory
 operator|.
@@ -426,10 +428,62 @@ name|literal
 argument_list|)
 return|;
 block|}
-else|else
+catch|catch
+parameter_list|(
+name|RuntimeException
+name|e
+parameter_list|)
 block|{
-comment|//if no mapped class
-comment|//bypass the LiteralFactory and return the string representation
+name|log
+operator|.
+name|info
+argument_list|(
+literal|"Unable to convert Literal value {} to Java Class {} because of {} with message {}"
+argument_list|,
+operator|new
+name|Object
+index|[]
+block|{
+name|literal
+block|,
+name|mapping
+operator|.
+name|getMappedClass
+argument_list|()
+operator|.
+name|getSimpleName
+argument_list|()
+block|,
+name|e
+operator|.
+name|getClass
+argument_list|()
+operator|.
+name|getSimpleName
+argument_list|()
+block|,
+name|e
+operator|.
+name|getMessage
+argument_list|()
+block|}
+argument_list|)
+expr_stmt|;
+name|log
+operator|.
+name|trace
+argument_list|(
+literal|"Exception:"
+argument_list|,
+name|e
+argument_list|)
+expr_stmt|;
+comment|//STANBOL-698: Decide what to do in such cases
+comment|//(a) throw an exception
+comment|// throw e;
+comment|//(b) ignore illegal values
+comment|//return null;
+comment|//(c) use the lexical form
 return|return
 name|literal
 operator|.
@@ -440,14 +494,27 @@ block|}
 block|}
 else|else
 block|{
-comment|//if dataType is not supported
-comment|/*                      * this could indicate two things:                      * 1) the SimpleLiteralFactory supports a new DataType and                      *    because of that it creates Literals with this Type                      * 2) Literals with this data type where created by other                      *    applications.                      * In the first case one need to update the enumeration. In                      * the second case using the LexicalForm should be OK                      * Rupert Westenthaler 2010.10.28                      */
+comment|// if no mapped class
+comment|// bypass the LiteralFactory and return the string
+comment|// representation
+return|return
+name|literal
+operator|.
+name|getLexicalForm
+argument_list|()
+return|;
+block|}
+block|}
+else|else
+block|{
+comment|// if dataType is not supported
+comment|/*                      * this could indicate two things: 1) the SimpleLiteralFactory supports a new DataType and                      * because of that it creates Literals with this Type 2) Literals with this data type                      * where created by other applications. In the first case one need to update the                      * enumeration. In the second case using the LexicalForm should be OK Rupert Westenthaler                      * 2010.10.28                      */
 name|log
 operator|.
 name|warn
 argument_list|(
-literal|"Missing Mapping for DataType "
-operator|+
+literal|"Missing Mapping for DataType {} -> return String representation"
+argument_list|,
 name|literal
 operator|.
 name|getDataType
@@ -455,8 +522,6 @@ argument_list|()
 operator|.
 name|getUnicodeString
 argument_list|()
-operator|+
-literal|" -> return String representation"
 argument_list|)
 expr_stmt|;
 return|return
@@ -474,14 +539,12 @@ name|log
 operator|.
 name|warn
 argument_list|(
-literal|"Unsupported Resource Type "
-operator|+
+literal|"Unsupported Resource Type {} -> return String by using the toString method"
+argument_list|,
 name|value
 operator|.
 name|getClass
 argument_list|()
-operator|+
-literal|" -> return String by using the toString method"
 argument_list|)
 expr_stmt|;
 return|return
