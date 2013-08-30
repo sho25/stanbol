1e8cc4ec2adf176a0dcd21667d902ae369f294a3
Rupert Westenthaler <rwesten@apache.org>
2013-08-30 12:09:18 +0000

    STANBOL-1151: The Sentiment Word Classifier now supports Lexical Categories. To ease implementation of classifiers a new utility class 'WordSentimentDictionary' was added.
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk@1518948 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 64135839b33b1ff1cd549126d8d656256c0f02a6

--- enhancement-engines/sentiment-wordclassifier/src/main/java/org/apache/stanbol/enhancer/engines/sentiment/api/SentimentClassifier.java
+++ enhancement-engines/sentiment-wordclassifier/src/main/java/org/apache/stanbol/enhancer/engines/sentiment/api/SentimentClassifier.java
@@ -23,6 +23,34 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Set
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|stanbol
+operator|.
+name|enhancer
+operator|.
+name|nlp
+operator|.
+name|pos
+operator|.
+name|LexicalCategory
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -52,7 +80,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * A simple classifier that determines for a given word a positive or negative   * value between -1 and 1. Unknown words will return the value 0.  *<p/>  * This Interface need to be implemented by Sentiment frameworks so that they  * can be used with this engine. Implementations need to be   * {@link BundleContext#registerService(String, Object, java.util.Dictionary)  * registered as OSGI service}.  * @author Sebastian Schaffert  */
+comment|/**  * A simple classifier that determines for a given word a positive or negative   * value between -1 and 1. Unknown words will return the value 0.  *<p/>  * This Interface need to be implemented by Sentiment frameworks so that they  * can be used with this engine. Implementations need to be   * {@link BundleContext#registerService(String, Object, java.util.Dictionary)  * registered as OSGI service}.  * @see LexicalCategoryClassifier  *   * @author Sebastian Schaffert  * @author Rupert Westenthaler  */
 end_comment
 
 begin_interface
@@ -60,28 +88,25 @@ specifier|public
 interface|interface
 name|SentimentClassifier
 block|{
-comment|/**      * Given the word passed as argument, return a value between -1 and 1 indicating its sentiment value from      * very negative to very positive. Unknown words should return the value 0.      *      * @param word      * @return      */
+comment|/**      * Given the word passed as argument, return a value between -1 and 1 indicating its sentiment value from      * very negative to very positive. Unknown words should return the value 0.      *      * @param cat the lexical category of the word (see       *<a href="https://issues.apache.org/jira/browse/STANBOL-1151">STANBOL-1151</a>)      * @param word the word      * @return      */
 specifier|public
 name|double
 name|classifyWord
 parameter_list|(
+name|LexicalCategory
+name|cat
+parameter_list|,
 name|String
 name|word
 parameter_list|)
 function_decl|;
-comment|/**      * Helper method. Return true if the given POS tag indicates an adjective in the language implemented by      * this classifier.      *      * @param posTag      * @return      */
+comment|/**      * Getter for the LexicalCategories for the parsed {@link PosTag}. Used      * to lookup the lexical categories for the       * {@link #classifyWord(LexicalCategory, String)} lookups.<p>      * Simple implementations might return {@link PosTag#getCategories()}. But      * as some {@link PosTag} instances might only define the literal      * {@link PosTag#getTag()} value this method might also implement its own      * mappings.      * @param posTag the posTag      * @return the categories       */
 specifier|public
-name|boolean
-name|isAdjective
-parameter_list|(
-name|PosTag
-name|posTag
-parameter_list|)
-function_decl|;
-comment|/**      * Helper method. Return true if the given POS tag indicates a noun in the language implemented by this      * classifier.      *      * @param posTag      * @return      */
-specifier|public
-name|boolean
-name|isNoun
+name|Set
+argument_list|<
+name|LexicalCategory
+argument_list|>
+name|getCategories
 parameter_list|(
 name|PosTag
 name|posTag
