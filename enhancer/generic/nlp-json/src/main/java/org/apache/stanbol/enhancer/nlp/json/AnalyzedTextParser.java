be4ea44a67d6d8b3881f741ce3597c1afc1e3d53
Rupert Westenthaler <rwesten@apache.org>
2013-10-22 06:11:29 +0000

    STANBOL-1132: re-add dependency additions to the trunk. Those got somehow lost in the new trunk (STANBOL-1094)
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk@1534507 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 510b97977f9acb816b325cd9a5aa183c7d17200f

--- enhancer/generic/nlp-json/src/main/java/org/apache/stanbol/enhancer/nlp/json/AnalyzedTextParser.java
+++ enhancer/generic/nlp-json/src/main/java/org/apache/stanbol/enhancer/nlp/json/AnalyzedTextParser.java
@@ -107,16 +107,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|List
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|Map
 operator|.
 name|Entry
