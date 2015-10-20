33a39735e24359f28ce808b1499f08c3f456726b
Rupert Westenthaler <rwesten@apache.org>
2015-10-20 07:42:49 +0000

    STANBOL-1443: merged fix from 0.12.1 to trunk
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk@1709524 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 294736f24053744253d911706f2770a9e75ae0be

--- enhancement-engines/lucenefstlinking/src/main/java/org/apache/stanbol/enhancer/engines/lucenefstlinking/IndexConfiguration.java
+++ enhancement-engines/lucenefstlinking/src/main/java/org/apache/stanbol/enhancer/engines/lucenefstlinking/IndexConfiguration.java
@@ -1310,13 +1310,7 @@ init|=
 name|index
 operator|.
 name|getSearcher
-argument_list|(
-literal|true
-argument_list|,
-literal|true
-argument_list|,
-literal|null
-argument_list|)
+argument_list|()
 decl_stmt|;
 try|try
 block|{
