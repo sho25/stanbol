0121f86d03ca3ebfad086137c2e5e0636a0920a1
Rupert Westenthaler <rwesten@apache.org>
2016-02-23 09:17:59 +0000

    merged fix for STANBOL-1449 to trunk
    git-svn-id: https://svn.apache.org/repos/asf/stanbol/trunk@1731813 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d5c27b0614cdf1a3da7816172e03909e0ed7ddff

---
+++ enhancement-engines/dbpedia-spotlight/src/test/java/org/apache/stanbol/enhancer/engines/dbpspotlight/TestDefaults.java
@@ -0,0 +1,34 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|stanbol
+operator|.
+name|enhancer
+operator|.
+name|engines
+operator|.
+name|dbpspotlight
+package|;
+end_package
+
+begin_interface
+specifier|public
+interface|interface
+name|TestDefaults
+block|{
+comment|//TODO: add here a public service that can be used for tests if one becomes
+comment|//      available again
+name|String
+name|DEFAULT_SPL_URL
+init|=
+literal|null
+decl_stmt|;
+block|}
+end_interface
+
+end_unit
+
